6f8589c7f1fcbfa1db6a3aa8a4abf7201ceebcaf
unknown <Naver@AD01033125.(none)>
2014-06-06 17:38:57 +0900

    basic...

---
+++ src/main/java/com/test/khs/App.java
@@ -0,0 +1,13 @@
+package com.test.khs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
